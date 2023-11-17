eclipse-oniro-mirrors/third_party_mesa3d

commit 5a3d6e01730ad5bab5d5832efbc99530059a4b1f

adpt OH
Signed-off-by: zleoyu <zhangleiyu1@huawei.com>

src/gallium/drivers/panfrost/pan_screen.c
@@ -124,6 +124,7 @@ panfrost_get_param(struct pipe_screen *screen, enum pipe_cap param)
         case PIPE_CAP_FRAMEBUFFER_NO_ATTACHMENT:
         case PIPE_CAP_QUADS_FOLLOW_PROVOKING_VERTEX_CONVENTION:
         case PIPE_CAP_SHADER_PACK_HALF_FLOAT:
+        case PIPE_CAP_NATIVE_FENCE_FD:
                 return 1;
 
         case PIPE_CAP_MAX_RENDER_TARGETS:
@@ -787,6 +788,16 @@ panfrost_fence_reference(struct pipe_screen *pscreen,
         }
 
         *ptr = fence;
+}
+
+static int
+panfrost_fence_get_fd(struct pipe_screen *_screen,
+                       struct pipe_fence_handle *fence)
+{
+        struct panfrost_device *dev = pan_device(_screen);
+        int fd = -1;
+        drmSyncobjExportSyncFile(dev->fd, fence->syncobj, &fd);
+        return fd;
 }
 
 static bool

meson.build
@@ -346,10 +346,11 @@ with_platform_x11 = _platforms.contains('x11')
 with_platform_wayland = _platforms.contains('wayland')
 with_platform_haiku = _platforms.contains('haiku')
 with_platform_windows = _platforms.contains('windows')
+with_platform_ohos = _platforms.contains('ohos')
 
 with_glx = get_option('glx')
 if with_glx == 'auto'
-  if with_platform_android
+  if with_platform_android or with_platform_ohos
     with_glx = 'disabled'
   elif with_dri
     with_glx = 'dri'
@@ -482,7 +483,7 @@ endif
 
 if with_egl
   _platforms += 'surfaceless'
-  if with_gbm and not with_platform_android
+  if with_gbm and not with_platform_android and not with_platform_ohos
     _platforms += 'drm'
   endif
 
@@ -1006,7 +1007,18 @@ if with_platform_android
   ]
 endif
 
-prog_python = import('python').find_installation('python3')
+if with_platform_ohos
+  pre_args += '-DHAVE_OHOS_PLATFORM'
+  dep_ohos = [
+    dependency('libsurface'),
+    dependency('libhilog'),
+  ]
+  c_args += '-I../ohos'
+endif
+
+#prog_python = import('python').find_installation('python3')
+prog_python = '/usr/bin/python3'
+
 has_mako = run_command(
   prog_python, '-c',
   '''
@@ -1681,6 +1693,10 @@ foreach d : _libdrm_checks
     )
   endif
 endforeach
+
+dep_hilog = dependency(
+  'libhilog'
+)
 
 with_gallium_drisw_kms = false
 dep_libdrm = dependency(

src/egl/drivers/dri2/egl_dri2.c
@@ -71,6 +71,8 @@
 #include "util/driconf.h"
 #include "util/u_math.h"
 
+#include "ohos_log.h"
+
 #define NUM_ATTRIBS 12
 
 static const struct dri2_pbuffer_visual {
@@ -1928,6 +1930,7 @@ dri2_surface_get_dri_drawable(_EGLSurface *surf)
 static _EGLProc
 dri2_get_proc_address(const char *procname)
 {
+   DISPLAY_LOGI("procname = %{public}s", procname);
    return _glapi_get_proc_address(procname);
 }
 

src/egl/drivers/dri2/egl_dri2.h
@@ -75,6 +75,10 @@ struct zwp_linux_dmabuf_feedback_v1;
 #include <system/window.h>
 #endif
 
+#ifdef HAVE_OHOS_PLATFORM
+#include "window.h"
+#endif
+
 #endif /* HAVE_ANDROID_PLATFORM */
 
 #include "eglconfig.h"
@@ -364,9 +368,9 @@ struct dri2_egl_surface
    } color_buffers[4], *back, *current;
 #endif
 
-#ifdef HAVE_ANDROID_PLATFORM
-   struct ANativeWindow *window;
-   struct ANativeWindowBuffer *buffer;
+#if defined(HAVE_ANDROID_PLATFORM) || defined (HAVE_OHOS_PLATFORM)
+   struct NativeWindow *window;
+   struct NativeWindowBuffer *buffer;
 
    /* in-fence associated with buffer, -1 once passed down to dri layer: */
    int in_fence_fd;
@@ -380,7 +384,7 @@ struct dri2_egl_surface
     */
    int color_buffers_count;
    struct {
-      struct ANativeWindowBuffer *buffer;
+      struct NativeWindowBuffer *buffer;
       int age;
    } *color_buffers, *back;
    uint32_t gralloc_usage;
@@ -556,6 +560,17 @@ static inline EGLBoolean
 dri2_initialize_android(_EGLDisplay *disp)
 {
    return _eglError(EGL_NOT_INITIALIZED, "Android platform not built");
+}
+#endif
+
+#ifdef HAVE_OHOS_PLATFORM
+EGLBoolean
+dri2_initialize_ohos(_EGLDisplay *disp);
+#else
+static inline EGLBoolean
+dri2_initialize_ohos(_EGLDisplay *disp)
+{
+   return _eglError(EGL_NOT_INITIALIZED, "ohos platform not built");
 }
 #endif
 

src/egl/drivers/dri2/platform_ohos.c
@@ -104,7 +104,7 @@ static int get_fourcc(int native)
         case PIXEL_FMT_RGBX_8888:
             return DRM_FORMAT_XBGR8888;
         default:
-            _eglLog(_EGL_WARNING, "unsupported native buffer format 0x%x", native);
+            _eglLog(_EGL_WARNING, "unsupported native buffer format 0x%{public}x", native);
     }
     return -1;
 }
@@ -372,7 +372,7 @@ ohos_create_surface(_EGLDisplay *disp, EGLint type, _EGLConfig *conf,
         dri2_surf->color_buffers_count = buffer_count;
 
         if (format != dri2_conf->base.NativeVisualID) {
-            _eglLog(_EGL_WARNING, "Native format mismatch: 0x%x != 0x%x",
+            _eglLog(_EGL_WARNING, "Native format mismatch: 0x%{public}x != 0x%{public}x",
                     format, dri2_conf->base.NativeVisualID);
         }
 
@@ -439,13 +439,13 @@ ohos_destroy_surface(_EGLDisplay *disp, _EGLSurface *surf)
     }
 
     if (dri2_surf->dri_image_back) {
-        _eglLog(_EGL_DEBUG, "%s : %d : destroy dri_image_back", __func__, __LINE__);
+        _eglLog(_EGL_DEBUG, "%{public}s : %{public}d : destroy dri_image_back", __func__, __LINE__);
         dri2_dpy->image->destroyImage(dri2_surf->dri_image_back);
         dri2_surf->dri_image_back = NULL;
     }
 
     if (dri2_surf->dri_image_front) {
-        _eglLog(_EGL_DEBUG, "%s : %d : destroy dri_image_front", __func__, __LINE__);
+        _eglLog(_EGL_DEBUG, "%{public}s : %{public}d : destroy dri_image_front", __func__, __LINE__);
         dri2_dpy->image->destroyImage(dri2_surf->dri_image_front);
         dri2_surf->dri_image_front = NULL;
     }
@@ -656,7 +656,7 @@ ohos_swap_buffers(_EGLDisplay *disp, _EGLSurface *draw)
     if (has_mutable_rb &&
         draw->RequestedRenderBuffer == EGL_SINGLE_BUFFER &&
         draw->ActiveRenderBuffer == EGL_SINGLE_BUFFER) {
-        _eglLog(_EGL_DEBUG, "%s: remain in shared buffer mode", __func__);
+        _eglLog(_EGL_DEBUG, "%{public}s: remain in shared buffer mode", __func__);
         return EGL_TRUE;
     }
 
@@ -908,7 +908,7 @@ ohos_add_configs_for_visuals(_EGLDisplay *disp)
 
     for (int i = 0; i < ARRAY_SIZE(format_count); i++) {
         if (!format_count[i]) {
-            _eglLog(_EGL_DEBUG, "No DRI config supports native format 0x%x",
+            _eglLog(_EGL_DEBUG, "No DRI config supports native format 0x%{public}x",
                     visuals[i].format);
         }
     }
@@ -947,7 +947,7 @@ ohos_display_shared_buffer(__DRIdrawable *driDrawable, int fence_fd,
     struct ANativeWindowBuffer *old_buffer UNUSED = dri2_surf->buffer;
 
     if (!_eglSurfaceInSharedBufferMode(&dri2_surf->base)) {
-        _eglLog(_EGL_WARNING, "%s: internal error: buffer is not shared",
+        _eglLog(_EGL_WARNING, "%{public}s: internal error: buffer is not shared",
                 __func__);
         return;
     }
@@ -967,7 +967,7 @@ ohos_display_shared_buffer(__DRIdrawable *driDrawable, int fence_fd,
 
     if (ANativeWindow_queueBuffer(dri2_surf->window, dri2_surf->buffer,
                                   fence_fd)) {
-        _eglLog(_EGL_WARNING, "%s: ANativeWindow_queueBuffer failed", __func__);
+        _eglLog(_EGL_WARNING, "%{public}s: ANativeWindow_queueBuffer failed", __func__);
         close(fence_fd);
         return;
     }
@@ -980,7 +980,7 @@ ohos_display_shared_buffer(__DRIdrawable *driDrawable, int fence_fd,
         struct dri2_egl_display *dri2_dpy =
             dri2_egl_display(dri2_surf->base.Resource.Display);
 
-        _eglLog(_EGL_WARNING, "%s: ANativeWindow_dequeueBuffer failed", __func__);
+        _eglLog(_EGL_WARNING, "%{public}s: ANativeWindow_dequeueBuffer failed", __func__);
 
         dri2_surf->base.Lost = true;
         dri2_surf->buffer = NULL;
@@ -1168,7 +1168,7 @@ ohos_open_device(_EGLDisplay *disp, bool swrast)
 #endif
 
     num_devices = drmGetDevices2(0, devices, ARRAY_SIZE(devices));
-    _eglLog(_EGL_WARNING, "ohos_open_device %d", num_devices);
+    _eglLog(_EGL_WARNING, "ohos_open_device %{public}d", num_devices);
     if (num_devices < 0) {
         return EGL_FALSE;
     }
@@ -1183,7 +1183,7 @@ ohos_open_device(_EGLDisplay *disp, bool swrast)
         dri2_dpy->fd = loader_open_device(device->nodes[node_type]);
         if (dri2_dpy->fd < 0) {
             DISPLAY_LOGI();
-            _eglLog(_EGL_WARNING, "%s() Failed to open DRM device %s",
+            _eglLog(_EGL_WARNING, "%{public}s() Failed to open DRM device %{public}s",
                     __func__, device->nodes[node_type]);
             continue;
         }
@@ -1191,7 +1191,7 @@ ohos_open_device(_EGLDisplay *disp, bool swrast)
         /* If a vendor is explicitly provided, we use only that.
          * Otherwise we fall-back the first device that is supported.
          */
-        DISPLAY_LOGI("vendor_name %s", vendor_name);
+        DISPLAY_LOGI("vendor_name %{public}s", vendor_name);
         if (vendor_name) {
             if (ohos_filter_device(disp, dri2_dpy->fd, vendor_name)) {
                 /* Device does not match - try next device */
@@ -1222,7 +1222,7 @@ ohos_open_device(_EGLDisplay *disp, bool swrast)
     DISPLAY_LOGI();
     if (dri2_dpy->fd < 0) {
         DISPLAY_LOGI();
-        _eglLog(_EGL_WARNING, "Failed to open %s DRM device",
+        _eglLog(_EGL_WARNING, "Failed to open %{public}s DRM device",
                 vendor_name ? "desired" : "any");
         return EGL_FALSE;
     }

src/egl/main/eglapi.c
@@ -408,9 +408,9 @@ _eglGetPlatformDisplayCommon(EGLenum platform, void *native_display,
       break;
 #endif
 #ifdef HAVE_OHOS_PLATFORM
-   case EGL_PLATFORM_OHOS_KHR:
-      disp = _eglGetOHOSDisplay(native_display, attrib_list);
-      break;
+    case EGL_PLATFORM_OHOS_KHR:
+        disp = _eglGetOHOSDisplay(native_display, attrib_list);
+        break;
 #endif
    case EGL_PLATFORM_DEVICE_EXT:
       disp = _eglGetDeviceDisplay(native_display, attrib_list);

src/egl/main/egldisplay.c
@@ -68,6 +68,7 @@
 #include <windows.h>
 #endif
 
+
 /**
  * Map build-system platform names to platform types.
  */

src/egl/meson.build
@@ -136,12 +136,10 @@ if with_dri2
       override_for_egl += 'cpp_std=c++17'
     endif
   endif
-
   if with_platform_ohos
     deps_for_egl += dep_ohos
     files_egl += files('drivers/dri2/platform_ohos.c')
   endif
-
 elif with_platform_haiku
   incs_for_egl += inc_haikugl
   c_args_for_egl += [

commit edff4bc006f6814394791971c0870d36038d18cf

panfrost: assign panfrost_fence_get_fd() to ->fence_get_fd
Fixes: 5a3d6e01730a ("adpt OH")
Signed-off-by: Yue Hu <huyue2@coolpad.com>

src/gallium/drivers/panfrost/pan_screen.c
@@ -926,6 +926,7 @@ panfrost_create_screen(int fd, struct renderonly *ro)
         screen->base.get_compiler_options = panfrost_screen_get_compiler_options;
         screen->base.fence_reference = panfrost_fence_reference;
         screen->base.fence_finish = panfrost_fence_finish;
+        screen->base.fence_get_fd = panfrost_fence_get_fd;
         screen->base.set_damage_region = panfrost_resource_set_damage_region;
 
         panfrost_resource_screen_init(&screen->base);